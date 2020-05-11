3f0964abbe7f5b2a90a1e60509666b40a944b79d
Michael Cooper <55610025+mcooper7649@users.noreply.github.com>
2020-05-11 10:35:16 -0400

    personalization
    updated color; h1;  footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Copyright ⓒ {year} MyCodeDojo.com</p>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <header>
       <h1>
         <HighlightIcon />
-        Keeper
+        Cooper's Keeper
       </h1>
     </header>
   );
